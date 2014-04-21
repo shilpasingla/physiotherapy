1daf1440c1fcd80cc3b91019e4f624585b8f8c34
Shilpa Singla <shilpasingla4@gmail.com>
2014-04-21 22:54:38 +0530

    basic project skelton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Physiotherapy::Application.config.secret_key_base = '57241c113d7b7bcca8065b66ebee267ffdca710ac920396b1a4eb9aa5682f736c2fb5454c405e400c3cd000d1de31e89a6e06d3dd5cdaade8b11f7fbf1618345'
